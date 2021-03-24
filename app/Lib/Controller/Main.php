edd5d984df0d84f1ed9bbe6cc071cedfac47c7a1
trad3r <c1mba@yandex.ru>
2021-03-24 10:03:08 +0300

    Правка ролей

--- app/Lib/Controller/Main.php
+++ app/Lib/Controller/Main.php
@@ -3,6 +3,8 @@
 
 namespace App\Controller;
 
+use App\Assets\AssetHelper;
+
 abstract class Main extends BaseController
 {
     public function beforeAction($action)
@@ -10,12 +12,11 @@ abstract class Main extends BaseController
         if (!parent::beforeAction($action)) {
             return false;
         }
-        return $this->redirect(['admin/site/index']);
 
-//        if(!$this->getRequest()->isAjax()) {
-//            AssetHelper::init($this->view);
-//        }
+        if(!$this->getRequest()->isAjax()) {
+            AssetHelper::init($this->view);
+        }
 
-//        return true;
+        return true;
     }
 }
